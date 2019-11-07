c71f3d55e510934faadd351d2d7d3203b8312b0f
Vicillian <44278914+Vicillian@users.noreply.github.com>
2019-11-06 18:28:17 -0800

    The computer can now choose Rock,Paper, or scissors.

--- js/script.js
+++ js/script.js
@@ -7,10 +7,64 @@
 $("#shoot").click(function(){
     let usermove = $("#input").val();
     $("#userChoice").append(usermove);
-    let cpmove = $("Rock").val();
+    console.log(usermove);
+    let cpmove = Math.floor((Math.random())*3)
+    console.log(cpmove)
+    if (cpmove === 0) {
+        $("#computerChoice").html("Rock");
+    }
+    else if (cpmove === 1) {
+        $("#computerChoice").html("Paper");
+    }
+    else if (cpmove === 2) {
+        $("#computerChoice").html("Scissor");
+    }
 
+  if (usermove === "Rock" && cpmove === 0) {
+    $("#result").html("Tie!");
+  }
+  else if (usermove === "Rock" && cpmove === 1) {
+    $("#result").html("Tie!");
+  }
+  if (usermove === "Rock" && cpmove === 2) {
+    $("#result").html("Tie!");
+  }
 });
 
 
 // DOCUMENT READY FUNCTION BELOW
+// *************************** YOUR CODE BELOW *******************************
+//******************TEST EARLY AND OFTEN USING console.log() ******************
+//****************** SERIOUSLY TEST USING console.log()!!! ******************
 
+//GLOBAL VARIABLES
+/* global $ */
+$("#shoot").click(function(){
+    let usermove = $("#input").val();
+    $("#userChoice").append(usermove);
+    console.log(usermove);
+    let cpmove = Math.floor((Math.random())*3)
+    console.log(cpmove)
+    if (cpmove === 0) {
+        $("#computerChoice").html("Rock");
+    }
+    else if (cpmove === 1) {
+        $("#computerChoice").html("Paper");
+    }
+    else if (cpmove === 2) {
+        $("#computerChoice").html("Scissor");
+    }
+
+  if (usermove === "Rock" && cpmove === 0) {
+    $("#result").html("Tie!");
+  }
+  else if (usermove === "Rock" && cpmove === 1) {
+    $("#result").html("Tie!");
+  }
+  if (usermove === "Rock" && cpmove === 2) {
+    $("#result").html("Tie!");
+  }
+});
+
+
+// DOCUMENT READY FUNCTION BELOW
